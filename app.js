bcbd15a20e4404041e76c382e6320e047d76fc9a
Moynul Haque <87459752+Moynul7@users.noreply.github.com>
2021-09-28 23:47:21 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+
+//love calculator start
+
+let fristname = prompt('What is your name?');
+let Partnername = prompt('What is your Partner name?');
+
+// calculation
+
+let number = Math.random() * 100;
+number = Math.floor(number) + 1;
+
+//showing result
+
+document.write(fristname + ' and' + ' ' + Partnername + 's love percenttage is' + ' ' + number + '%');
+
